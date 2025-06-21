e4bd86ac970b6a04f715447405cedfa362919db8
nurgelli <nt.interpretor@gmail.com>
2025-06-21 22:14:33 +0500

    added static files

--- config/settings/base.py
+++ config/settings/base.py
@@ -14,8 +14,11 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parents[2]
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
+# print(BASE_DIR)
+# print(BASE_DIR / 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
