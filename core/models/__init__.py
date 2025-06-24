e5ba825aa3b211ef29ceaacddcbdc472b2a82be3
nurgelli <nt.interpretor@gmail.com>
2025-06-24 14:16:09 +0500

    models created

---
+++ core/models/__init__.py
@@ -0,0 +1,9 @@
+from .project import *
+from .activity_log import *
+from .attachment import *
+from .comment import *
+from .project import *
+from .reminder import *
+from .subtask import *
+from . tag import *
+from .task import *
