43f004444c458af4ce51d1022e9f167aa1b06d5c
nurgelli <nt.interpretor@gmail.com>
2025-07-24 14:04:13 +0500

    Tested

--- core/forms.py
+++ core/forms.py
@@ -2,20 +2,6 @@ from django import forms
 from .models import Project, Task, SubTask, Comment, Attachment, Reminder
 
 
-class TaskForm(forms.ModelForm):
-    class Meta:
-        model = Task
-        fields = ['title', 'description', 'due_date', 'priority', 'project']
-        widgets = {
-            'due_date': forms.DateInput(attrs={'type': 'datetime-local'}),
-            'priority': forms.Select(),
-        }
-        
-    def __init__(self, *args, **kwargs):
-        super(TaskForm, self).__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'form-control'
-
 class ProjectForm(forms.ModelForm):
     
     class Meta:
@@ -26,4 +12,20 @@ class ProjectForm(forms.ModelForm):
         super(ProjectForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
+            
+            
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'due_date', 'priority']
+        widgets = {
+            'due_date': forms.DateInput(attrs={'type': 'datetime-local'}),
+            'priority': forms.Select(),
+        }
+        
+    def __init__(self, *args, **kwargs):
+        super(TaskForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
         
